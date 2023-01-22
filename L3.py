c38f3f85ec179e52b132ef481f28c826ed0926a8
Karina_Belxhenko <karin.bel4enko@yandex.ru>
2023-01-22 15:50:07 +0300

    Сделала д/з

--- L3.py
+++ L3.py
@@ -7,10 +7,18 @@
 #-> 1
 
 N = int(input('Кол. чисел:'))
-a=[int(input('Ввести число:')) for i in range(N)]
-x=int(input('Заданное число:'))
-b=[abs(a[i]-x) for i in range(len(a))]
-print(sum(map(lambda z: int(z == x), a)))
+list1 = [int(input('Ввести число:')) for i in range(N)]
+k = int(input('Заданное число:'))
+
+inc = 0
+for i in list1:
+    if i == N:
+        inc += 1
+print(inc)
+
+ 
+#b=[abs(a[i]-x) for i in range(len(a))]
+#print(sum(map(lambda z: int(z == x), a)))'''
 
 
 #Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
@@ -23,11 +31,17 @@ print(sum(map(lambda z: int(z == x), a)))
 #-> 5
 
 '''N = int(input('Кол. чисел:'))
-a=[int(input('Ввести число:')) for i in range(N)]
-x=int(input('Заданное число:'))
-b=[abs(a[i]-x) for i in range(len(a))]
-print(a[b.index(min(b))])'''
+list1 = [int(input('Ввести число:')) for i in range(N)]
+k = int(input('Заданное число:'))
+m = abs(k - list1[0])
+number = list1[0] 
 
+for i in range(1, len(list1)):
+	if m > abs(k - list1[i]):
+		m = abs(k - list1[i])
+		number = list1[i]
+
+print(number)'''
 
 #Задача 20: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность. В случае с английским алфавитом очки распределяются так:
 #A, E, I, O, U, L, N, S, T, R – 1 очко;
@@ -51,25 +65,23 @@ print(a[b.index(min(b))])'''
 #Вывод:
 #12
 
-'''import re
-def isCyrillic(text):#
-	return bool(re.search('[а-яА-Я]', text))
-points_en = {1:'AEIOULNSTR',
-      	2:'DG',
-      	3:'BCMP',
-      	4:'FHVWY',
-      	5:'K',
-      	8:'JZ',
-      	10:'QZ'}
-points_ru = {1:'АВЕИНОРСТ',
-      	2:'ДКЛМПУ',
-      	3:'БГЁЬЯ',
-      	4:'ЙЫ',
-      	5:'ЖЗХЦЧ',
-      	8:'ШЭЮ',
-      	10:'ФЩЪ'}
+'''points_en = {1:'AEIOULNSTR', 2:'DG', 3:'BCMP', 4:'FHVWY', 5:'K', 8:'JZ', 10:'QZ'}
+points_ru = {1:'АВЕИНОРСТ', 2:'ДКЛМПУ', 3:'БГЁЬЯ', 4:'ЙЫ', 5:'ЖЗХЦЧ', 8:'ШЭЮ', 10:'ФЩЪ'}
+
 text = input().upper()
-if isCyrillic(text):
-	print(sum([k for i in text for k, v in points_ru.items() if i in v]))
+count = 0
+
+for i in text:
+	if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+		for j in points_en:
+			if i in points_en[j]:
+				count += j  
 else:
-	print(sum([k for i in text for k, v in points_en.items() if i in v]))'''
+       for j in points_ru:
+	        if i in points_ru[j]:
+                 count += j  
+	
+print(count)'''
+
+	
+
